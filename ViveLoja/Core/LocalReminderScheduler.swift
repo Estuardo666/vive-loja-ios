e0a08e743d846ee2b1821faa6d9968cc17791de1
Viveloja <estuarlito@gmail.com>
2026-09-01 05:51:38 -0500

    fix(notifications): annotate async notification continuation

--- ViveLoja/Core/LocalReminderScheduler.swift
+++ ViveLoja/Core/LocalReminderScheduler.swift
@@ -25,7 +25,7 @@ final class LocalReminderScheduler {
     }
 
     private func add(_ request: UNNotificationRequest) async throws {
-        try await withCheckedThrowingContinuation { continuation in
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
             center.add(request) { error in
                 if let error { continuation.resume(throwing: error) }
                 else { continuation.resume(returning: ()) }
